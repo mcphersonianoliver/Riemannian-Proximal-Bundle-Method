c5f607ce76f046382fff56d066331e3d7473ca34
Ian McPherson <mcphersonianoliver@gmail.com>
2025-04-21 17:50:57 -0400

    set up the structure of code - need to fix initialization parameters to be able to start running first experiments

--- experiments.py
+++ experiments.py
@@ -1,1 +1,77 @@
-# %% 
+# %% Experiment 0: checking if manopt is installed correctly - running simple example
+import autograd.numpy as anp
+import pymanopt
+
+anp.random.seed(42)
+
+dim = 3
+manifold = pymanopt.manifolds.Sphere(dim)
+
+matrix = anp.random.normal(size=(dim, dim))
+matrix = 0.5 * (matrix + matrix.T)
+
+@pymanopt.function.autograd(manifold)
+def cost(point):
+    return -point @ matrix @ point
+
+problem = pymanopt.Problem(manifold, cost)
+
+optimizer = pymanopt.optimizers.SteepestDescent()
+result = optimizer.run(problem)
+
+eigenvalues, eigenvectors = anp.linalg.eig(matrix)
+dominant_eigenvector = eigenvectors[:, eigenvalues.argmax()]
+
+print("Dominant eigenvector:", dominant_eigenvector)
+print("Pymanopt solution:", result.point)
+
+# %% Experiment 1: PSD matrices
+from pymanopt.manifolds import SymmetricPositiveDefinite
+
+# set the manifold a priori
+dim = 3
+manifold = SymmetricPositiveDefinite(dim)
+
+samples = 10 
+# generate |samples| random PSD matrices
+data = []
+for _ in range(samples):
+    data_point = manifold.random_point()
+    data.append(data_point)
+
+# Riemannian median cost function
+def cost(point, data):
+    # riemannian median cost function given points
+
+    return sum([manifold.dist(point, x) for x in data]) / len(data)
+
+# %%
+
+print("Data shape:", len(data), data[0].shape)
+# check if the matrices are PSD
+for i, matrix in enumerate(data):
+    eigenvalues, _ = anp.linalg.eig(matrix)
+    print(f"Matrix {i} eigenvalues:", eigenvalues)
+    assert all(eigenvalues >= 0), f"Matrix {i} is not PSD"
+
+# print matrices
+for i, matrix in enumerate(data):
+    print(f"Matrix {i}:\n", matrix)
+
+# %%
+point_a, point_b = data[0], data[1]
+
+dist_ab = manifold.dist(point_a, point_b)
+print("Distance between point_a and point_b:", dist_ab)
+# %%
+
+
+# %%
+# create a random point on the manifold
+point = manifold.random_point()
+print("Random point on manifold:", point)
+
+objective = cost(point,data)
+print("Objective value:", objective)
+
+# %%
--- src/RiemannianProximalBundle.py
+++ src/RiemannianProximalBundle.py
@@ -1,64 +1,188 @@
+import numpy as np
+
 class RProximalBundle(object):
-    def __init__(self, manifold = manifold, retraction = retraction,
-                 transport = transport, proximal_parameter = proximal_parameter,
-                 trust_parameter = trust_parameter,
-                 true_objective = true_objective, initial_point = initial_point,
+    '''Riemannian proximal bundle algorithm for non-convex optimization on Riemannian manifolds.
+    This class implements the proximal bundle algorithm for non-convex optimization on Riemannian manifolds.
+    The algorithm is based on the proximal bundle method for convex optimization, which is a generalization of the bundle method for non-convex optimization.
+    This implementation centers on a two-cut surrogate model, however, one may easily replace with other convex surrogates, satisfying certain properties.'''
+
+    def __init__(self, manifold, retraction, retraction_error = 0,
+                 transport_map = transport_map, transport_error = 0, proximal_parameter = 1,
+                 trust_parameter = 0.1,
+                 objective_function, initial_point = initial_point, initial_objective = initial_objective,
                  initial_subgradient = initial_subgradient, minimizer = minimizer,
                  max_iter = max_iter, tolerance = tolerance):
 
         # parameters and tools
         self.manifold = manifold
         self.retraction_map = retraction 
-        self.transport_map = transport
+        self.transport_map = transport_map
 
         self.current_proximal_center = initial_point
         self.proximal_parameter = proximal_parameter
 
         self.trust_parameter = trust_parameter
-        self.objective_function = true_objective
+        self.objective_function = objective_function
+        self.initial_objective = initial_objective
+
+        # flag for if the current surrogate is a single-cut (first iterate at the proximal center)
+        self.single_cut = True
+
+        # parameters for first-order retraction/transport
+        self.retraction_error = retraction_error
+        self.transport_error = transport_error
+        
+        self.subgradient_at_center = initial_subgradient
+
+        # storage for two-cut model - setting aside memory
+        self.model_subg = [initial_subgradient] # s_{k}, initialize together to line-up indexes, dummy entry
+        self.untransported_subgradients = [initial_subgradient] # g_{k}
+        self.transported_subgradients = [initial_subgradient] # \hat{g}_{k}
+        self.candidate_directions = [] # d_{k}
+        self.candidate_obj_history = [initial_objective] # f(R_x(x_{k}))
+        self.candidate_model_obj_history = [] # f_k(d_{k})
+        self.error_shifts = [0] # e_{f_k}(\rho_{k})
 
         # storage for algorithm run
-        self.objective_gap = []
         self.proximal_parameter_history = []
-        self.candidate_point_history = []
-        self.proximal_center_history = []
         self.indices_of_descent_steps = []
     
         # algorithm run parameters
         self.max_iter = max_iter
         self.tolerance = tolerance
         self.initial_point = initial_point
-        self.initial_subgradent = initial_subgradient
+        self.initial_subgradient = initial_subgradient
         self.minimizer = minimizer
         
-        # storage for aggregate cut surrogate models
-        self.subgradients = [initial_subgradient]
-        self.affine_shifts =[true_objective(initial_point)]
 
     def run(self, T):
         ''' run the proximal bundle algorithm'''
-        
         for i in range(self.max_iter):
             # compute the candidate direction and convert to a point on the manifold using retraction map
-            candidate_direction = self.cand_prox_direction() # IMPLEMENT
-            candidate_point = self.direction_to_point(candidate_direction)
-            new_subgradient = self.manifold.subgradient(candidate_point) # IMPLEMENT
+            candidate_direction = self.cand_prox_direction() 
+            self.candidate_directions.append(candidate_direction)
+            candidate_point = self.direction_to_point(candidate_direction) # unnecessary
+
+            # compute true objective and predicted objective
+            true_objective = self.objective_function(candidate_point)
+            self.candidate_obj_history.append(true_objective)
+            model_objective = self.model_evaluation(candidate_direction, i) # check index
+            self.candidate_model_obj_history.append(model_objective)
+            
+            # query new subgradient
+            new_subgradient = self.manifold.subgradient(candidate_point) # TODO: IMPLEMENT
+
+            self.single_cut = False # no longer single-cut model after taking steps
 
             # compute the model's predicted objective gap versus the true objective gap
-            ratio = self.model_versus_true(candidate_direction, candidate_point)
-            if ratio > self.trust_parameter:
-                self.descent_step(new_subgradient, candidate_point)
+            ratio = self.model_versus_true(candidate_direction, candidate_point, i)
+
+            if ratio > self.trust_parameter: # DESCENT STEP
+                self.current_proximal_center = candidate_point # moves model
+
+                # update proximal center and set initial subgradient 
+                self.current_proximal_center = candidate_point   
+                self.subgradient_at_center = new_subgradient # updates subgradient at center
+
+                # update model information - one-cut model now!
+                self.untransported_subgradients.append(new_subgradient) # g_{k+1}
+                self.transported_subgradients.append(new_subgradient) # no transport is done
+                self.model_subg.append(new_subgradient) # one-cut model - placeholder for index purposes
+                self.error_shifts.append(0) # e_{f_k}(\rho_{k+1}) = 0, no transport is done
+                
+                # for visualization
                 self.indices_of_descent_steps.append(i)
-                self.proximal_center_history.append(candidate_point)
+                self.proximal_parameter_history.append(self.proximal_parameter)
             else:
-                self.null_step(new_subgradient, candidate_point)
-        raise NotImplementedError("The method 'run' is not yet implemented.")
+                if self.proximal_parameter_check(candidate_direction): # NULL STEP 
+                    # don't move - just update model
+
+                    self.untransported_subgradients.append(new_subgradient) # g_{k+1}
+                    self.transported_subgradients.append(self.transport_map(new_subgradient, self.current_proximal_center)) # \hat{g}_{k+1}
+                    
+                    model_subg = 1 # TODO: implement this
+                    self.model_subg.append(model_subg) # one-cut model - placeholder for index purposes
+                    
+                    error_shift = self.compute_shift_adjustment(new_subgradient)
+                    self.error_shifts.append(error_shift) # conservative shift adjustment
+
+                    # for visualization
+                    self.proximal_parameter_history.append(self.proximal_parameter)
+
+                else: # PROXIMAL PARAMETER DOUBLING STEP
+                    self.proximal_parameter *= 2 # double proximal parameter
+
+                    # don't update model and don't move - repeat previous model
+                    prev_untransport_subg = self.untransported_subgradients[-1] # g_{k+1}
+                    prev_transport_subg = self.transported_subgradients[-1] # \hat{g}_{k+1}
+                    prev_model_subg = self.model_subg[-1] # s_{k+1}
+                    prev_error_shift = self.error_shifts[-1] # e_{f_k}(\rho_{k+1})
+
+                    self.untransported_subgradients.append(prev_untransport_subg) # g_{k+1}
+                    self.transported_subgradients.append(prev_transport_subg) # \hat{g}_{k+1}
+                    self.model_subg.append(prev_model_subg) # s_{k+1}
+                    self.error_shifts.append(prev_error_shift) # e_{f_k}(\rho_{k+1})
+
+                    # for visualization
+                    self.proximal_parameter_history.append(self.proximal_parameter)
 
 ## helper functions for proximal bundle algorithm run
-    def cand_prox_direction(self):
-        ''' compute the proximal direction in current tangent space'''
+    def model_evaluation(self, direction, iter):
+        ''' compute the cut surrogate model
+        Paramters
+        ----------
+        direction : np.array
+            tangent vector on the manifold to be assessed
+        iter : int
+            which model to be used for evaluation
+            
+        Returns
+        -------
+        model_objective : float
+            the objective value of the model at the candidate point'''
+        # TODO: CURRENT VERSION ASSUMES TWO-CUT MODEL
+        # recalls model information for given iteration
+        candidate_dir = self.candidate_directions[iter]
+        transp_subg = self.transported_subgradients[iter]
+        mod_subg = self.prev_model_info[iter]
+        mod_shift = self.error_shifts[iter]
+        true_obj_cand = self.candidate_obj_history[iter]
+        model_obj_cand = self.candidate_model_obj_history[iter]
 
-        raise NotImplementedError("The method 'proximal_direction' is not yet implemented.")
+        # computes on "new" cut
+        affine_new_shift = true_obj_cand - np.dot(transp_subg, candidate_dir) - mod_shift
+        new_inner = np.dot(transp_subg, direction)
+        new_cut_obj = affine_new_shift + new_inner 
+        
+        # computes on "old" cut
+        affine_old_shift = model_obj_cand - np.dot(mod_subg, candidate_dir)
+        old_inner = np.dot(mod_subg, direction)
+        old_cut_obj = affine_old_shift + old_inner
+
+        # returns the maximum of the two cuts
+        return max(new_cut_obj, old_cut_obj)
+
+
+    def cand_prox_direction(self):
+        ''' compute proximal direction due to model '''
+
+        if self.single_cut:
+            # computes the proximal direction using the single-cut model: - (g_{k}/\rho_{k})
+            return - (self.untransported_subgradients[-1]/self.proximal_parameter)
+        else:
+            # computes form from Lemma 10 of paper - compute \theta_{t+1}
+            prev_true_obj_cand = self.candidate_obj_history[-1]
+            prev_error_shift = self.error_shifts[-1]
+            prev_model_obj_cand = self.candidate_model_obj_history[-1]
+            prev_model_subg = self.model_subg[-1]
+            prev_transported_subg = self.transported_subgradients[-1]
+            numerator = self.proximal_parameter * (prev_true_obj_cand - prev_error_shift - prev_model_obj_cand)
+            denominator = np.linalg.norm(prev_transported_subg - prev_model_subg)**2
+            convex_comb_arg = numerator / denominator
+            convex_comb = min(1, convex_comb_arg)
+
+            # computes the proxmial direction - convex combination of two subg
+            return - ((convex_comb * prev_transported_subg + (1 - convex_comb) * prev_model_subg) / self.proximal_parameter)
 
     def direction_to_point(self, candidate_direction):
         ''' conver the tangent vector to a point on the manifold'''
@@ -66,60 +190,44 @@ class RProximalBundle(object):
         self.candidate_point_history.append(candidate_point)
         return candidate_point
 
-    def model_versus_true(self, candidate_direction, candidate_point):
+    def model_versus_true(self, candidate_direction, candidate_point, iter):
         ''' computes the model's predicted objective gap versus the true objective gap'''
 
         numerator = self.objective_function(self.proximal_center_history[-1]) - self.objective_function(candidate_point) # computes true gap on the manifold
-        denominator = self.objective_function(self.proximal_center_history[-1]) - self.cut_surrogate_evaluation(candidate_direction) # computes model gap on the tangent space
+        denominator = self.objective_function(self.proximal_center_history[-1]) - self.model_evaluation(candidate_direction, iter) # computes model gap on the tangent space
         ratio = numerator / denominator # computes the ratio of the two gaps
         return ratio
-
-    def descent_step(self, subgradient, candidate_point):
-        ''' throw away previous model and start a new local surrogate at candidate iterate, move to candidate iterate'''
-        # construct new surrogate at new tangent space
-        self.subgradients = [subgradient]
-        self.affine_shifts = [self.objective_function(candidate_point)]
-
-        # updates new proximal center 
-        self.current_proximal_center = candidate_point
-
-    def null_step(self, subgradient, candidate_point):
-        ''' incorporate subgradient information from candidate iterate to the current surrogate model, don't move'''
-        
-        # transport subgradient to the current proximal center
-        transported_subgradient = self.transport_map(subgradient, self.current_proximal_center, candidate_point)
-
-        # compute the affine shift of the surrogate model
-        # TODO : this should be a function of the transported subgradient
-        
-        self.update_surrogate_model()
-
-        
-        raise NotImplementedError("The method 'null_step' is not yet implemented.")
-
-## helper for cut surrogate model - this should be removed for generality later 
-    def cut_surrogate_evaluation(self):
-        ''' compute the cut surrogate model'''
-
-        raise NotImplementedError("The method 'cut_surrogate_model' is not yet implemented.")
     
-    def update_surrogate_model(self):
-        ''' update the surrogate model'''
+    def proximal_parameter_check(self, candidate_direction, new_subgradient):
+        ''' compare proximal gap with shift'''
 
-    def add_cut_two_cut_model(self):
-        ''' add the cut to the surrogate model'''
+        # compute the proximal gap and shift adjustment
+        shift_adjustment = self.compute_shift_adjustment(new_subgradient)
+        model_proximal_gap = self.compute_model_proximal_gap(candidate_direction)
 
-        raise NotImplementedError("The method 'add_cut_two_cut_model' is not yet implemented.")
+        check_value = 0.5*model_proximal_gap - (shift_adjustment / (1-self.trust_parameter))
 
-    def descent_step_cut_model(self):
-        ''' throw away previous model and start a new local surrogate at candidate iterate, move to candidate iterate'''
+        if check_value >= 0:
+            return True
+        else:
+            return False
+
+    def compute_shift_adjustment(self, new_subgradient):
+        # compute relevant subgradient norms
+        norm_subgradient_center = np.linalg.norm(self.subgradient_at_center)
+        norm_new_subgradient = np.linalg.norm(new_subgradient)
+
+        radius_of_cand = ((2 * norm_subgradient_center) / self.proximal_parameter) + self.retraction_error * (((2 * norm_subgradient_center) / self.proximal_parameter)**2)
+        shift_adjustment = self.transport_error * norm_new_subgradient * radius_of_cand**2
+        return shift_adjustment
+    
+    def compute_model_proximal_gap(self, candidate_direction):
+        current_location_objective = self.objective_function(self.proximal_center_history[-1])
+        prox_obj_on_model = self.model_evaluation(candidate_direction) + (self.proximal_parameter/2)*(np.linalg.norm(candidate_direction)**2)
         
-        raise NotImplementedError("The method 'descent_step_cut_model' is not yet implemented.")
+        prox_gap = current_location_objective - prox_obj_on_model
+        return prox_gap
 
-    def prox_step_cut_model(self):
-        ''' throw away previous model and start a new local surrogate at candidate iterate, move to candidate iterate'''
-        
-        raise NotImplementedError("The method 'prox_step_cut_model' is not yet implemented.")
 
 
 ## helper functions for visualizations
@@ -132,11 +240,3 @@ class RProximalBundle(object):
         ''' plot the proximal parameter versus iteration number'''
         
         raise NotImplementedError("The method 'plot_proximal_parameter_versus_iter' is not yet implemented.")
-
-    
-        
-        
-
-
-        
-
